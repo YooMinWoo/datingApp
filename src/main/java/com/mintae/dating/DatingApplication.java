8a78a94ee761fd8a22295c5ef425910e54001317
YooMinWoo <alsn99@codeline.kr>
2024-11-08 17:46:19 +0900

    version 1.0

---
+++ src/main/java/com/mintae/dating/DatingApplication.java
@@ -0,0 +1,14 @@
+package com.mintae.dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // 시큐리티 기능 off
+public class DatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingApplication.class, args);
+	}
+
+}
